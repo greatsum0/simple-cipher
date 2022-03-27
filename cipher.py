16eb8447f09c581d36262ebdbb4eabd3a5350080
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-03-27 12:54:15 +0300

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -6,16 +6,16 @@ import math
 class Cipher:
     
     def __init__(self, key, conversion_table):
-        self.key = key
+        self.key = self._preprocess_text(key)
         self.conversion_table = np.array(conversion_table)
 
     def encrypt(self, plain_text):
-        return self._encrypt_second_phase(self._encrypt_first_phase(plain_text))
+        return self._encrypt_second_phase(self._encrypt_first_phase(plain_text))[0]
 
     def decrypt(self, cipher_text):
-        return self._decrypt_second_phase(self._decrypt_first_phase(cipher_text))
+        return self._decrypt_second_phase(self._decrypt_first_phase(cipher_text))[0]
 
-    def _preprocess_plain_text(self, plain_text):
+    def _preprocess_text(self, plain_text):
         return ("".join(re.findall("[A-Za-z]+",plain_text))).upper()
 
     def _extend_key(self, plain_text):
@@ -26,7 +26,7 @@ class Cipher:
             return extended_key   
 
     def _encrypt_first_phase(self, plain_text):
-        plain_text = self._preprocess_plain_text(plain_text)
+        plain_text = self._preprocess_text(plain_text)
         self.extended_key = self._extend_key(plain_text)
         cipher = []
 
@@ -41,14 +41,14 @@ class Cipher:
     def _encrypt_second_phase(self, cipher_text):
         len_column = math.ceil(len(cipher_text)/2)
         cipher_text = cipher_text + "0"*abs(len(cipher_text) - len_column * 2)
-        return "".join(np.array(list(cipher_text)).reshape((2,len_column)).T.reshape(1,-1).squeeze().tolist())
+        return "".join(np.array(list(cipher_text)).reshape((2,len_column)).T.reshape(1,-1).squeeze().tolist()), np.array(list(cipher_text)).reshape((2,len_column))
         
 
     def _decrypt_first_phase(self, cipher_text):
-        len_column = len(cipher_text)//2
+        len_column = math.ceil(len(cipher_text)/2)
         decipher = "".join(
             (np.array(list(cipher_text)).reshape(len_column,2).T).reshape(1,-1).squeeze())
-        return self._preprocess_plain_text(decipher)
+        return self._preprocess_text(decipher), np.array(list(cipher_text)).reshape(len_column,2).T
         
 
     def _decrypt_second_phase(self, cipher_text):
